d80d7b9ea592d874401f4f085d99fc048adcb0cb
Satyajit Thadeshwar <sthadeshwar@netflix.com>
2015-07-25 14:45:16 -0700

    Adding equals() and hashCode() to ActionInstance class

--- scheduled-actions-core/src/main/java/com/netflix/scheduledactions/ActionInstance.java
+++ scheduled-actions-core/src/main/java/com/netflix/scheduledactions/ActionInstance.java
@@ -182,6 +182,20 @@ public class ActionInstance {
         return id != null ? id : name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ActionInstance that = (ActionInstance) o;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
     public static class ActionInstanceBuilder {
         private String id;
         private String name;
