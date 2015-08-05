3e0712a5ebfe3abb6450bfc779600b961bd4fc4e
Satyajit Thadeshwar <sthadeshwar@netflix.com>
2015-08-04 23:09:03 -0700

    Adding timezone to CronTrigger

--- scheduled-actions-core/src/main/java/com/netflix/scheduledactions/triggers/CronTrigger.java
+++ scheduled-actions-core/src/main/java/com/netflix/scheduledactions/triggers/CronTrigger.java
@@ -15,17 +15,19 @@ public class CronTrigger implements Trigger {
 
     private final String cronExpression;
     private final Date startAt;
+    private final String timeZoneId;
+
+    public CronTrigger(String cronExpression) {
+        this(cronExpression, "America/Los_Angeles", new Date());
+    }
 
     @JsonCreator
     public CronTrigger(@JsonProperty("cronExpression") String cronExpression,
+                       @JsonProperty("timeZoneId") String timeZoneId,
                        @JsonProperty("startAt") Date startAt) {
         this.cronExpression = cronExpression;
+        this.timeZoneId = timeZoneId;
         this.startAt = startAt;
-    }
-
-    public CronTrigger(String cronExpression) {
-        this.cronExpression = cronExpression;
-        this.startAt = new Date();
     }
 
     @Override
@@ -37,7 +39,7 @@ public class CronTrigger implements Trigger {
     public com.netflix.fenzo.triggers.Trigger<Context> createFenzoTrigger(Context context,
                                                                           Class<? extends Action1<Context>> action) {
         return new com.netflix.fenzo.triggers.CronTrigger<>(
-            this.cronExpression, this.startAt, context.getName(), context, Context.class, action
+            this.cronExpression, this.timeZoneId, this.startAt, context.getName(), context, Context.class, action
         );
     }
 
